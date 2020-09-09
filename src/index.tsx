aa13b8df31ae14570a8b389b98b3730cd635844f
tiagofernando-dev <pro.dev.work@hotmail.com>
2020-09-10 04:42:48 +0900

    Complete todo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware, Store } from 'redux'
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(
-  <React.StrictMode>
+import './index.css'
+import App from './App'
+import reducer from './store/reducer'
+import * as serviceWorker from './serviceWorker'
+
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType
+} = createStore(reducer, applyMiddleware(thunk))
+
+const rootElement = document.getElementById("root")
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>,
+  rootElement
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
