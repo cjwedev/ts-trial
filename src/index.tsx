d3d0b103e055eef870dd6b9e82394e8e17d1d491
tiagofernando-dev <pro.dev.work@hotmail.com>
2020-09-10 06:30:32 +0900

    Add redux-devtools-extension

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore, applyMiddleware, Store } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
@@ -11,7 +12,7 @@ import * as serviceWorker from './serviceWorker'
 
 const store: Store<ArticleState, ArticleAction> & {
   dispatch: DispatchType
-} = createStore(reducer, applyMiddleware(thunk))
+} = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const rootElement = document.getElementById("root")
 render(
